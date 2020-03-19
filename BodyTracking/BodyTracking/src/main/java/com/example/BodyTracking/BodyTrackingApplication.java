ddd001ce472376840618781d3a28c50c3fe41a3c
Luís Santana <luis.santana@ua.pt>
2020-03-19 18:14:59 +0000

    Created project

---
+++ BodyTracking/BodyTracking/src/main/java/com/example/BodyTracking/BodyTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.example.BodyTracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodyTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BodyTrackingApplication.class, args);
+	}
+
+}
