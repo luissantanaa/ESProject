d797dbac12b891eefae019f45951204ee5665a9e
Alexandre <alexandresantos1@ua.pt>
2020-03-21 11:58:28 +0000

    added templates andpackages

--- BodyTracking/BodyTracking/src/main/java/proj/es/BodyTracking/BodyTrackingApplication.java
+++ BodyTracking/BodyTracking/src/main/java/proj/es/BodyTracking/BodyTrackingApplication.java
@@ -1,4 +1,4 @@
-package com.example.BodyTracking;
+package proj.es.BodyTracking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- BodyTracking/BodyTracking/src/test/java/proj/es/BodyTracking/BodyTrackingApplicationTests.java
+++ BodyTracking/BodyTracking/src/test/java/proj/es/BodyTracking/BodyTrackingApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.BodyTracking;
+package proj.es.BodyTracking;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
